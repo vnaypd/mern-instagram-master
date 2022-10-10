7c9db467727cc299ee730ad0a5c6951fb908d3c1
bunai <80148425+vnaypd@users.noreply.github.com>
2022-10-10 21:17:26 +0530

    hb

--- mern_insta_backend/server.js
+++ mern_insta_backend/server.js
@@ -1,1 +1,23 @@
+ import express from 'express';
+ import cors from 'cors';
+ import mongoose from 'mongoose';
+ import Pusher from 'pusher';
  
+
+ //app config
+const app=express()
+const port=process.env.PORT || 9000
+
+ //middleware
+
+
+ //db config
+
+
+ //api routes
+
+
+ //listen
+
+
+
