caa4fefadba7d7a107f385e7eb46cbfbf15d6d98
bunai <80148425+vnaypd@users.noreply.github.com>
2022-10-10 22:12:16 +0530

    j

--- mern_insta_backend/server.js
+++ mern_insta_backend/server.js
@@ -8,13 +8,22 @@
 const app=express()
 const port=process.env.PORT || 9000
 
+
  //middleware
 app.use(express.json())
 app.use(cors())
 
 
  //db config
+const connection_url='mongodb+srv://binoy:1Elr5dZhrdNzyvsv@cluster0.kw5invo.mongodb.net/instagram?retryWrites=true&w=majority'
+mongoose.connect(connection_url,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+    })
 
+mongoose.connection.once('open',()=>{
+    console.log('DB connected bro enjoy the app')
+})
 
  //api routes
 app.get('/',(req,res)=>res.status(200).send('hello world'))
